6c44403aee4a3b9a893a79c11970d99dad43f8e6
Emily DuBois <emilymdubois@gmail.com>
2017-08-10 10:35:15 -0400

    Adds quotes

--- index.js
+++ index.js
@@ -164,7 +164,7 @@ const WebhookPassthroughMethod = (lambda) => ({
       ],
       Uri: cf.sub(`arn:aws:apigateway:\${AWS::Region}:lambda:path/2015-03-31/functions/\${${lambda}.Arn}/invocations`),
       RequestTemplates: {
-        'application/json': "{\"headers\":$input.params(),\"body\":$input.json('$')}"
+        'application/json': "{\"headers\":\"$input.params()\",\"body\":$input.json('$')}"
       }
     },
     MethodResponses: [
